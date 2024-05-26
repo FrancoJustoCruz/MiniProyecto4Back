07c6009e714d1dd7e10b5ae3a424b3388f76d875
justocruzfranco <justocruzfranco@gmail.com>
2024-05-26 13:42:46 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { swaggerUi, specs } from './swagger.js';
+
+import authRoutes from './routes/auth.routes.js';
+import taskRoutes from './routes/task.routes.js';
+
+const app = express();
+
+
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
+
+export default app;
